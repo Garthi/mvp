1d1ee5a47f23a4d2ea592b42facdc9d38d6bb089
Garthi <garth@new-crusader.de>
2016-08-21 21:32:34 +0200

    ...

--- app/src/main/java/chuck/norris/jokes/App.java
+++ app/src/main/java/chuck/norris/jokes/App.java
@@ -6,28 +6,31 @@ import android.util.Log;
 import chuck.norris.jokes.model.JokeAPI;
 import retrofit.RestAdapter;
 
-public class App extends Application {
-
-    private static App instance;
+public class App extends Application
+{
     private static JokeAPI serverAPI;
 
     @Override
-    public void onCreate() {
+    public void onCreate()
+    {
         super.onCreate();
-        instance = this;
+
         serverAPI = new RestAdapter.Builder()
                 .setEndpoint(JokeAPI.ENDPOINT)
                 .setLogLevel(RestAdapter.LogLevel.FULL)
-                .setLog(new RestAdapter.Log() {
+                .setLog(new RestAdapter.Log()
+                {
                     @Override
-                    public void log(String message) {
+                    public void log(String message)
+                    {
                         Log.v("Retrofit", message);
                     }
                 })
                 .build().create(JokeAPI.class);
     }
 
-    public static JokeAPI getServerAPI() {
+    public static JokeAPI getServerAPI()
+    {
         return serverAPI;
     }
 }
